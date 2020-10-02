1133c244b2279ab5604f171ddd01fb253467d8d8
yumi takahashi <i-c16h10n202-26227@users.noreply.github.com>
2020-10-02 19:20:29 +0900

    SQL文の修正

--- php-rt-iine/retweet.php
+++ php-rt-iine/retweet.php
@@ -3,10 +3,10 @@
 //RTした記事の有無の確認
 if (isset($_POST['rt_post_id'])) {
     $retweet = $db->prepare(
-        'SELECT members.name,posts.*
+        'SELECT members.name,posts.* 
         FROM members,posts
-        WHERE members.id=posts.member_id AND posts.id = ?'
-    );
+        WHERE members.id=posts.member_id AND posts.rt_post_id = ?'
+    ); //流用しているからデータ*にしているけど減らしたほうがいいのか?
     $retweet->execute([$_POST['rt_post_id']]);
     $retweeted = $retweet->fetch();
 
@@ -14,25 +14,28 @@ if (isset($_POST['rt_post_id'])) {
         //既にRTした記事がある場合、消去
         $rt_delete = $db->prepare(
             'UPDATE posts
-            SET rt_post_id=0, modified=NOW()
-            WHERE post_id=?'
+            SET rt_post_id=0
+            WHERE member_id=?,rt_post_id=?'
         );
-        $rt_delete->execute([$_POST['rt_post_id']]);
+        $rt_delete->execute([$_SESSION['id'], $_POST['rt_post_id']]);
+
+        echo '削除しました';
     } else {
         //投稿
-        $rt_message_body = '@' . $_POST['rt_member'] . $_POST['rt_message'];
+        $rt_message_body = '@' . $_POST['rt_member'] . ' ' . $_POST['rt_message'];
 
         $rt_do = $db->prepare(
             'INSERT INTO posts 
-    SET member_id=?,message=?,rt_post_id=?,created=NOW()'
+            SET member_id=?,message=?,rt_post_id=?,created=NOW()'
         );
         $rt_do->execute([
             $_SESSION['id'],
             $rt_message_body,
             $_POST['rt_post_id']
         ]);
+        echo '投稿しました';
     }
 
-    header('Location: index.php');
-    exit();
+    // header('Location: index.php');
+    // exit();
 }
