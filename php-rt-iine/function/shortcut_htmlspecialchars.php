441913df62600813dc244921f64128a3e196b22e
yumi takahashi <i-c16h10n202-26227@users.noreply.github.com>
2020-10-20 19:35:43 +0900

    htmlspecialcharsの型宣言

--- php-rt-iine/function/shortcut_htmlspecialchars.php
+++ php-rt-iine/function/shortcut_htmlspecialchars.php
@@ -5,7 +5,7 @@
  * 
  * ENT_QUOTES,'UTF-8'含む
  */
-function h($value)
+function h(string $value = NULL)
 {
-    return htmlspecialchars(strVal($value), ENT_QUOTES, 'UTF-8');
+    return htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
 }
